27b861487c7becbfc8565c208fe487b44a429c6b
Letícia Cotta <leticiacotta@gmail.com>
2022-02-02 18:52:38 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countResidents(animal) {
+  const armazenador = {};
+  species.forEach((specie) => {
+    armazenador[specie.name] = specie.residents.length;
+  });
+  return armazenador;
 }
+
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return countResidents(animal);
+  }
+  if (animal.specie && animal.sex) {
+    return species
+      .find((specie) => specie.name === animal.specie)
+      .residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  if (animal.specie) {
+    return countResidents(animal)[animal.specie];
+  }
+}
+console.log(countAnimals());
 
 module.exports = countAnimals;
