0fd7d7e4c29f8700aa87fcf6b276344eefb40d58
Letícia Cotta <leticiacotta@gmail.com>
2022-02-03 13:31:02 -0300

    Tentativas. Requisito 10 correções

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -5,10 +5,13 @@ const { employees } = data;
 const { species } = data;
 
 function getEmployeesCoverage() {
+  // EXEMPLO PARA FIXAÇÃO:
+  // const peopleSearcher = (LETICIA) => employees.find((OBJETOATUAL) =>
+  //  OBJETOATUAL.firstName === LETICIA || OBJETOATUAL.lastName === LETICIA);
   const peopleSearcher = employees.find(
     (employee) =>
-      employees.firstName === employees.Name ||
-      employees.lastName === employees.Name,
+      employees.firstName === employee.Name ||
+      employees.lastName === employee.Name,
     employees.id,
   );
 }
