fa1eb0106b3ed9cba9dff775ab973cbcd0891682
Letícia Cotta <leticiacotta@gmail.com>
2022-02-03 12:09:20 -0300

    Tentativa 1

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,36 +3,35 @@ const data = require('../data/zoo_data');
 const { prices } = data;
 
 function countEntrants(entrants) {
-  const armazenador = {};
-  prices.forEach((entrant) => {
-    armazenador[entrants] === prices.child.lenght, prices.adult.lenght;
-    prices.senior.lenght;
+  const armazenador = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      armazenador.child += 1;
+    }
+    if (entrant.age >= 18 && entrant.age < 50) {
+      armazenador.adult += 1;
+    }
+    if (entrant.age >= 50) {
+      armazenador.senior += 1;
+    }
   });
   return armazenador;
-
-  if (entrants == 18) {
-    return (entrants.child);
-  }
-  if (entrants >= 18 && entrants <= 50) {
-    return (entrants.adult);
-  }
-  if (entrants >= 50){
-    return (entrants.senior);
-  }
 }
 
 function calculateEntry(entrants) {
-  let child = { entrants.child };
-  let senior = { entrants.senior };
-  let adult = { entrants.adult };
-  const totalSum = child + senior + adult;
+  const totalEntrants = countEntrants(entrants);
+  const totalCost =
+    prices.adult * totalEntrants.adult +
+    prices.child * totalEntrants.child +
+    prices.senior * totalEntrants.senior;
 
   if (prices === undefined || countEntrants[entrants] === undefined) {
     return countEntrants(' 0 '); // Retorna 0 se o argumento ou o objeto forem vazios
   } // fim do IF
-  if (prices === true) {
-    return countEntrants[entrants].lenght;
-  } // fim do IF
+  // if (prices === true) {
+  //   return countEntrants[entrants].lenght;
+  // } // fim do IF
+  return totalCost;
 }
 
 module.exports = { calculateEntry, countEntrants };
