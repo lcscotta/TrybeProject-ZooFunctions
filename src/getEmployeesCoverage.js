8dece178954db82c678e72b157972c9d19e37208
Letícia Cotta <leticiacotta@gmail.com>
2022-02-03 13:41:22 -0300

    Commit para desbugar os 7 exercicios anteriores

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,19 +1,19 @@
 const data = require('../data/zoo_data');
 
-const { employees } = data;
+// const { employees } = data;
 
-const { species } = data;
+// const { species } = data;
 
 function getEmployeesCoverage() {
-  // EXEMPLO PARA FIXAÇÃO:
-  // const peopleSearcher = (LETICIA) => employees.find((OBJETOATUAL) =>
-  //  OBJETOATUAL.firstName === LETICIA || OBJETOATUAL.lastName === LETICIA);
-  const peopleSearcher = employees.find(
-    (employee) =>
-      employees.firstName === employee.Name ||
-      employees.lastName === employee.Name,
-    employees.id,
-  );
+  // // EXEMPLO PARA FIXAÇÃO:
+  // // const peopleSearcher = (LETICIA) => employees.find((OBJETOATUAL) =>
+  // //  OBJETOATUAL.firstName === LETICIA || OBJETOATUAL.lastName === LETICIA);
+  // const peopleSearcher = employees.find(
+  //   (employee) =>
+  //     employees.firstName === employee.Name ||
+  //     employees.lastName === employee.Name,
+  //   employees.id,
+  // );
 }
 
 module.exports = getEmployeesCoverage;
