3f2e93b0aa9280acd49d568b5fa4ac1aaf61a794
Letícia Cotta <leticiacotta@gmail.com>
2022-02-07 03:01:16 -0300

    Requisito 10. Tentativa 2

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,19 +1,27 @@
 const data = require('../data/zoo_data');
 
-// const { employees } = data;
+const { employees } = data;
 
-// const { species } = data;
+const { species } = data;
 
 function getEmployeesCoverage() {
-  // // EXEMPLO PARA FIXAÇÃO:
-  // // const peopleSearcher = (LETICIA) => employees.find((OBJETOATUAL) =>
-  // //  OBJETOATUAL.firstName === LETICIA || OBJETOATUAL.lastName === LETICIA);
-  // const peopleSearcher = employees.find(
-  //   (employee) =>
-  //     employees.firstName === employee.Name ||
-  //     employees.lastName === employee.Name,
-  //   employees.id,
-  // );
+  // a primeira variável confere os dados dos funcionários.
+  const peopleSearcher = employees.find(
+    (employee) =>
+      employees.firstName === employee.Name ||
+      employees.lastName === employee.Name,
+    employees.id,
+  );
+  // a segunda variável confere os dados dos animais.
+  const animalSearcher = species.find(
+    (specie) =>
+      species.name === specie.name || species.id === employees.responsibleFor,
+  );
+  // condicional que nega dados inválidos
+  if (peopleSearcher === undefined) {
+    throw new Error('Informações inválidas');
+  } // fim do IF
+  return peopleSearcher;
 }
 
 module.exports = getEmployeesCoverage;
